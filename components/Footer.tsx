c98ffacaf7a5021e204a9129128a2814bb184f50
adrielk <theadrielkim@gmail.com>
2022-01-23 14:40:31 -0500

    footer padding

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import WideContentFrame from "../components/WideContentFrame";
 
 function Footer() {
   return (
-    <WideContentFrame extraStyle="bg-dark">
+    <WideContentFrame extraStyle="bg-dark px-2">
       <footer className="font-light text-white py-1">
         <small>
           Copyright &copy; {new Date().getFullYear()} Adriel Kim. All Rights
