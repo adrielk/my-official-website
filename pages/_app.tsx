ca2e47927f04049b67ff38e7ea8ca0beed70910f
adrielk <theadrielkim@gmail.com>
2022-02-09 09:52:40 -0500

    head meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,27 @@ import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 import HomeData from "../page_data/home.json";
 
+//to do:
+// Make SEO customizable!!! (right now it's hardcoded with your name lol)
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>{`${HomeData.name}'s Portfolio Website`}</title>
-        <meta property="og:title" content="My page title" key="title" />
+        <meta
+          property="og:title"
+          content="Adriel Kim's Portfolio"
+          key="title"
+        />
+        <meta
+          name="description"
+          content="Adriel is a Computer Science major at the University of Virginia open to full-time software engineering jobs. Check out what he's been working on!"
+        />
+        <meta
+          name="keywords"
+          content="Adriel Kim, Adriel, Kim, UVA, University of Virginia, Virginia, Computer Science, Engineering"
+        />
+        <img />
       </Head>
       <Navigation />
       <div className="pt-20">
